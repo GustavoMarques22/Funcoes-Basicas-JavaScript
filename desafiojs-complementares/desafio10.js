c408714f70e13dd6d19b65fe93c5d047e266a3b1
Gustavo <gustavo.marques@aluno.unievangelica.edu.br>
2025-05-02 08:30:24 -0300

    Add files via upload

---
+++ desafiojs-complementares/desafio10.js
@@ -0,0 +1,28 @@
+/*Desenvolva uma função que receba um array de objetos representando estudantes de um curso e um objeto que representa um estudante procurado.
+Depois retorne se o objeto está presente no array.*/
+
+// Função que verifica se um objeto está presente no array
+function verificarObjetoNoArray(arr, objeto) {
+    return arr.some(item => JSON.stringify(item) === JSON.stringify(objeto));
+}
+
+// Array de objetos representando alunos
+const alunos = [
+    { id: 1, nome: 'João', idade: 20 },
+    { id: 2, nome: 'Maria', idade: 22 },
+    { id: 3, nome: 'Pedro', idade: 21 },
+    { id: 4, nome: 'Ana', idade: 19 }
+];
+
+// Objeto que queremos verificar se está presente no array
+const alunoProcurado = { id: 2, nome: 'Maria', idade: 22 };
+
+// Utilizando a função para verificar a presença do objeto no array
+const objetoEstaPresente = verificarObjetoNoArray(alunos, alunoProcurado);
+
+// Exibindo o resultado
+if (objetoEstaPresente) {
+    console.log('O aluno está presente no array.');
+} else {
+    console.log('O aluno não está presente no array.');
+}
---
+++ desafiojs-complementares/desafio9.js
@@ -0,0 +1,6 @@
+/*Crie um array de strings e utilize includes para verificar se uma determinada string está presente.*/
+
+const palavras = ["apple", "banana", "orange", "grape"];
+console.log("A palavra 'banana' está no array?", palavras.includes("banana"));
+
+//saída esperada: "A palavra 'banana' está no array?" true
